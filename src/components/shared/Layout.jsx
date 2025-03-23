fa2f5635e7cbf4c84383fff3601f5e1802d68d73
Josue Igiraneza <igiranezaj28@gmail.com>
2025-03-23 20:41:20 +0200

    feat: add `poppins` and `geist` fonts

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -4,7 +4,7 @@ import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="flex h-screen bg-gray-100 font-geist">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
